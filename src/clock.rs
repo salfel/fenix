487757a900f4f3ef78da1459057b51eeb0577722
Felix Salcher <dev.felix.salcher@gmail.com>
2025-02-13 08:25:00 +0100

    remove offset function

--- src/clock.rs
+++ src/clock.rs
@@ -11,39 +11,35 @@ enum InterfaceClock {
     L4LS = 0x0,
 }
 
+#[repr(u32)]
+#[derive(Clone, Copy)]
 pub enum FunctionalClock {
-    Gpio1,
+    Gpio1 = 0xAC,
 }
 
 impl FunctionalClock {
-    fn clock_module(&self) -> ClockModule {
+    const fn clock_module(&self) -> ClockModule {
         match self {
             FunctionalClock::Gpio1 => ClockModule::CmPer,
         }
     }
 
-    fn offset(&self) -> u32 {
-        match self {
-            FunctionalClock::Gpio1 => 0xAC,
-        }
-    }
-
-    fn clkactivity_mask(&self) -> u32 {
+    const fn clkactivity_mask(&self) -> u32 {
         match self {
             FunctionalClock::Gpio1 => 1 << 19,
         }
     }
 
-    fn interface_clock(&self) -> InterfaceClock {
+    const fn interface_clock(&self) -> InterfaceClock {
         match self {
             FunctionalClock::Gpio1 => InterfaceClock::L4LS,
         }
     }
 
     pub fn enable(&self) {
-        write_addr(self.clock_module() as u32 + self.offset(), 0x2);
+        write_addr(self.clock_module() as u32 + *self as u32, 0x2);
 
-        while read_addr(self.clock_module() as u32 + self.offset()) & IDLEST_BITS != 0 {}
+        while read_addr(self.clock_module() as u32 + *self as u32) & IDLEST_BITS != 0 {}
 
         while read_addr(self.clock_module() as u32 + self.interface_clock() as u32)
             & self.clkactivity_mask()
