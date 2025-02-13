69013abed18b3fe6b1cee26d17e3f9f76d4669cb
Felix Salcher <dev.felix.salcher@gmail.com>
2025-02-13 10:56:53 +0100

    remove clkactivity check as it loops forever

--- src/clock.rs
+++ src/clock.rs
@@ -7,10 +7,6 @@ enum ClockModule {
     CmPer = 0x44E0_0000,
 }
 
-enum InterfaceClock {
-    L4LS = 0x0,
-}
-
 #[repr(u32)]
 #[derive(Clone, Copy)]
 pub enum FunctionalClock {
@@ -24,27 +20,10 @@ impl FunctionalClock {
         }
     }
 
-    const fn clkactivity_mask(&self) -> u32 {
-        match self {
-            FunctionalClock::Gpio1 => 1 << 19,
-        }
-    }
-
-    const fn interface_clock(&self) -> InterfaceClock {
-        match self {
-            FunctionalClock::Gpio1 => InterfaceClock::L4LS,
-        }
-    }
-
     pub fn enable(&self) {
         write_addr(self.clock_module() as u32 + *self as u32, 0x2);
 
         while read_addr(self.clock_module() as u32 + *self as u32) & IDLEST_BITS != 0 {}
-
-        while read_addr(self.clock_module() as u32 + self.interface_clock() as u32)
-            & self.clkactivity_mask()
-            == 0
-        {}
     }
 }
 
