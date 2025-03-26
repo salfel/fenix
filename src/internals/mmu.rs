674cdb251125022e04f36ef4af68be8b0806e93e
Felix Salcher <dev.felix.salcher@gmail.com>
2025-03-26 15:51:40 +0100

    create pagetableentry data structure

--- src/internals/mmu.rs
+++ src/internals/mmu.rs
@@ -1,7 +1,5 @@
 use core::arch::asm;
 
-const SECTION_PAGE_TABLE_BASE: u32 = 0xC02;
-
 pub fn initialize() {
     unsafe {
         invalidate_tlb();
@@ -13,13 +11,59 @@ pub fn initialize() {
     }
 }
 
+enum PageTableEntry {
+    Section(SectionPageTableEntry),
+}
+
+impl From<PageTableEntry> for u32 {
+    fn from(value: PageTableEntry) -> Self {
+        match value {
+            PageTableEntry::Section(section) => section.into(),
+        }
+    }
+}
+
+struct SectionPageTableEntry {
+    index: u32,
+    access_permissions: AccessPermissions,
+}
+
+impl From<SectionPageTableEntry> for u32 {
+    fn from(val: SectionPageTableEntry) -> Self {
+        let SectionPageTableEntry {
+            index,
+            access_permissions,
+        } = val;
+        {
+            let permissions: u32 = access_permissions.into();
+            (index << 20) | permissions | 0b10
+        }
+    }
+}
+
+enum AccessPermissions {
+    Full,
+}
+
+impl From<AccessPermissions> for u32 {
+    fn from(val: AccessPermissions) -> Self {
+        match val {
+            AccessPermissions::Full => 0b11 << 10,
+        }
+    }
+}
+
 unsafe fn invalidate_tlb() {
     asm!("mcr p15, 0, r1, c8, c7, 0")
 }
 
 unsafe fn setup_page_tables() {
     for i in 0..4096 {
-        level1_page_table[i as usize] = SECTION_PAGE_TABLE_BASE | (i << 20);
+        let section = PageTableEntry::Section(SectionPageTableEntry {
+            index: i,
+            access_permissions: AccessPermissions::Full,
+        });
+        level1_page_table[i as usize] = section.into();
     }
 }
 
@@ -28,7 +72,7 @@ unsafe fn initialize_ttbcr() {
 }
 
 unsafe fn initialize_ttbr0() {
-    let page_table_addr: u32 = level1_page_table.as_ptr() as u32;
+    let page_table_addr: u32 = &raw const level1_page_table as u32;
 
     asm!("mcr p15, 0, {0}, c2, c0, 0", in(reg) page_table_addr);
 }
