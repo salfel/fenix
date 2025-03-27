aa19e3b2f469f88d0a7c15048fc31d1b570ce68f
Felix Salcher <dev.felix.salcher@gmail.com>
2025-03-27 11:32:48 +0100

    setup level2 page table

--- src/internals/mmu.rs
+++ src/internals/mmu.rs
@@ -2,42 +2,74 @@ use core::arch::asm;
 
 pub fn initialize() {
     unsafe {
-        invalidate_tlb();
         setup_page_tables();
+        setup_level2_page_table();
         initialize_ttbcr();
         initialize_ttbr0();
+        invalidate_tlb();
         setup_domains();
         enable_mmu();
     }
 }
 
-enum PageTableEntry {
-    Section(SectionPageTableEntry),
+enum L1PageTableEntry {
+    Fault,
+    Section(L1SectionPageTableEntry),
+    Pointer(L1PointerTableEntry),
 }
 
-impl From<PageTableEntry> for u32 {
-    fn from(value: PageTableEntry) -> Self {
+impl L1PageTableEntry {
+    pub fn new_section(index: u32) -> Self {
+        L1PageTableEntry::Section(L1SectionPageTableEntry {
+            index,
+            access_permissions: AccessPermissions::Full,
+        })
+    }
+}
+
+impl From<L1PageTableEntry> for u32 {
+    fn from(value: L1PageTableEntry) -> Self {
         match value {
-            PageTableEntry::Section(section) => section.into(),
+            L1PageTableEntry::Fault => 0x0,
+            L1PageTableEntry::Section(section) => section.into(),
+            L1PageTableEntry::Pointer(pointer) => pointer.into(),
         }
     }
 }
 
-struct SectionPageTableEntry {
+struct L1SectionPageTableEntry {
     index: u32,
     access_permissions: AccessPermissions,
 }
 
-impl From<SectionPageTableEntry> for u32 {
-    fn from(val: SectionPageTableEntry) -> Self {
-        let SectionPageTableEntry {
+impl From<L1SectionPageTableEntry> for u32 {
+    fn from(val: L1SectionPageTableEntry) -> Self {
+        let L1SectionPageTableEntry {
             index,
             access_permissions,
         } = val;
-        {
-            let permissions: u32 = access_permissions.into();
-            (index << 20) | permissions | 0b10
-        }
+
+        let permissions: u32 = match access_permissions {
+            AccessPermissions::Full => 0b11 << 10,
+        };
+        (index << 20) | permissions | 0b10
+    }
+}
+
+struct L1PointerTableEntry {
+    table: *mut L2PageTable,
+}
+
+impl L1PointerTableEntry {
+    pub fn new(table: *mut L2PageTable) -> Self {
+        L1PointerTableEntry { table }
+    }
+}
+
+impl From<L1PointerTableEntry> for u32 {
+    fn from(val: L1PointerTableEntry) -> Self {
+        let L1PointerTableEntry { table } = val;
+        table as u32 | 0b01
     }
 }
 
@@ -45,13 +77,44 @@ enum AccessPermissions {
     Full,
 }
 
-impl From<AccessPermissions> for u32 {
-    fn from(val: AccessPermissions) -> Self {
-        match val {
-            AccessPermissions::Full => 0b11 << 10,
+#[repr(align(1024))]
+struct L2PageTable([u32; 1024]);
+
+impl L2PageTable {
+    const fn new() -> Self {
+        L2PageTable([0; 1024])
+    }
+}
+
+struct L2PageTableEntry {
+    address: u32,
+    permissions: AccessPermissions,
+}
+
+impl L2PageTableEntry {
+    pub fn new(address: u32) -> Self {
+        L2PageTableEntry {
+            address,
+            permissions: AccessPermissions::Full,
         }
     }
 }
+
+impl From<L2PageTableEntry> for u32 {
+    fn from(val: L2PageTableEntry) -> Self {
+        let L2PageTableEntry {
+            address,
+            permissions,
+        } = val;
+        let permissions: u32 = match permissions {
+            AccessPermissions::Full => 0b11 << 4,
+        };
+        address | permissions | 0b10
+    }
+}
+
+#[no_mangle]
+static mut LEVEL2_PAGE_TABLE: L2PageTable = L2PageTable::new();
 
 unsafe fn invalidate_tlb() {
     asm!("mcr p15, 0, r1, c8, c7, 0")
@@ -59,12 +122,20 @@ unsafe fn invalidate_tlb() {
 
 unsafe fn setup_page_tables() {
     for i in 0..4096 {
-        let section = PageTableEntry::Section(SectionPageTableEntry {
-            index: i,
-            access_permissions: AccessPermissions::Full,
-        });
+        let section = L1PageTableEntry::new_section(i);
         level1_page_table[i as usize] = section.into();
     }
+}
+
+unsafe fn setup_level2_page_table() {
+    const BASE_ADDRESS: u32 = 0x40300000;
+    let l1_pointer = L1PointerTableEntry::new(&raw mut LEVEL2_PAGE_TABLE);
+    for i in 0..1024 {
+        let entry = L2PageTableEntry::new(BASE_ADDRESS | i << 12);
+        LEVEL2_PAGE_TABLE.0[i as usize] = entry.into();
+    }
+
+    level1_page_table[BASE_ADDRESS as usize >> 20] = L1PageTableEntry::Pointer(l1_pointer).into();
 }
 
 unsafe fn initialize_ttbcr() {
