4642bf13415d430254c0831417ef30a6a1090f00
Felix Salcher <dev.felix.salcher@gmail.com>
2025-04-02 17:26:01 +0200

    make peripheral memory priveleged so it can't be accessed from user mode

--- src/internals/mmu/l1.rs
+++ src/internals/mmu/l1.rs
@@ -11,7 +11,7 @@ pub fn initialize() {
     let kernel_memory: Range<u32> = 0x4020_0000..0x4040_0000;
 
     enable_memory_range(kernel_memory, AccessPermissions::UserReadOnly);
-    enable_memory_range(peripheral_memory, AccessPermissions::Full);
+    enable_memory_range(peripheral_memory, AccessPermissions::Privileged);
 }
 
 fn enable_memory_range(range: Range<u32>, permissions: AccessPermissions) {
