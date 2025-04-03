803b75a2af7eab5a03132399e9f3924ffd056b03
Felix Salcher <dev.felix.salcher@gmail.com>
2025-04-03 22:54:54 +0200

    fix bug where stack would overflow

--- libfenix/src/sysclock.rs
+++ libfenix/src/sysclock.rs
@@ -1,23 +1,33 @@
 use crate::Syscall;
-use core::arch::asm;
+use core::arch::global_asm;
 
 pub fn millis() -> u32 {
     let syscall = Syscall::Millis;
     syscall.call().unwrap()
 }
 
+global_asm!("
+    wait_store:
+        stmfd sp!, {{r0-r12, lr}}
+
+        mov r2, r0
+
+        mrs r0, cpsr
+        push {{r0}}
+
+        mov r0, sp
+        mov r1, lr
+        mov r2, r2
+        svc #0x1
+");
+
 pub fn wait(ms: u32) {
     let until = millis() + ms;
-    let sp: u32;
-    let pc: u32;
     unsafe {
-        asm!("stmfd sp!, {{r0-r12, lr}}", "mrs r0, cpsr", "push {{r0}}", "mov r0, sp", out("r0") sp, out("lr") pc);
+        wait_store(until);
     }
+}
 
-    let syscall = Syscall::Yield {
-        sp,
-        pc,
-        until: Some(until),
-    };
-    syscall.call();
+extern "C" {
+    fn wait_store(ms: u32);
 }
