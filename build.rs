7e04177fc14fd01c88c80ff4da3d29805cd3b1ff
Felix Salcher <dev.felix.salcher@gmail.com>
2025-04-03 21:33:19 +0200

    watch programs folder for any changed files

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 fn main() {
     println!("cargo:rerun-if-changed=src/asm/exceptions.S");
     println!("cargo:rerun-if-changed=src/asm/setup.S");
@@ -6,4 +8,13 @@ fn main() {
     println!("cargo:rerun-if-changed=src/asm/interrupts.S");
     println!("cargo:rerun-if-changed=src/asm/mmu.S");
     println!("cargo:rerun-if-changed=boot/linker.ld");
+
+    println!("cargo:rerun-if-changed=programs");
+
+    for entry in fs::read_dir("programs").unwrap() {
+        let path = entry.unwrap().path();
+        if path.is_file() {
+            println!("cargo:rerun-if-changed=programs/{}", path.display());
+        }
+    }
 }
