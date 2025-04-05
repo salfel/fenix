f87918b4930be7fd8b8faf2053e0749e4e1ed73f
Felix Salcher <dev.felix.salcher@gmail.com>
2025-04-05 10:33:05 +0200

    move kernel to subcrate to not mess up linker arguments for user subcrate

---
+++ kernel/build.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn main() {
+    println!("cargo:rerun-if-changed=kernel/src/asm/exceptions.S");
+    println!("cargo:rerun-if-changed=kernel/src/asm/setup.S");
+    println!("cargo:rerun-if-changed=kernel/src/asm/kernel.S");
+    println!("cargo:rerun-if-changed=kernel/src/asm/software_interrupts.S");
+    println!("cargo:rerun-if-changed=kernel/src/asm/interrupts.S");
+    println!("cargo:rerun-if-changed=kernel/src/asm/mmu.S");
+    println!("cargo:rerun-if-changed=kernel/boot/linker.ld");
+
+    println!("cargo:rerun-if-changed=../programs");
+
+    for entry in fs::read_dir("../programs").unwrap() {
+        let path = entry.unwrap().path();
+        if path.is_file() {
+            println!("cargo:rerun-if-changed=../programs/{}", path.display());
+        }
+    }
+}
