a762c33e589ab4ba2c253bb2ce930bee72eedff1
Felix Salcher <dev.felix.salcher@gmail.com>
2025-04-10 20:36:52 +0200

    add some convenience functions to i2c::write

--- libfenix/src/i2c.rs
+++ libfenix/src/i2c.rs
@@ -5,8 +5,29 @@ pub fn begin(slave_address: u32) {
     syscall.call();
 }
 
+pub fn write(data: u8) {
+    let syscall = Syscall::I2cWrite {
+        data: &[data],
+    };
+    syscall.call();
+}
+
 pub fn write_buf(data: &[u8]) {
     let syscall = Syscall::I2cWrite { data };
+    syscall.call();
+}
+
+pub fn write_str(data: &str) {
+    let syscall = Syscall::I2cWrite {
+        data: data.as_bytes(),
+    };
+    syscall.call();
+}
+
+pub fn write_char(data: char) {
+    let syscall = Syscall::I2cWrite {
+        data: &[data as u8],
+    };
     syscall.call();
 }
 
