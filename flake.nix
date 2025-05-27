3f0b448f853f4f94d0c7b5b89750ff73489acce4
Felix Salcher <dev.felix.salcher@gmail.com>
2025-05-27 09:57:35 +0200

    update formatting

--- flake.nix
+++ flake.nix
@@ -1,26 +1,24 @@
 {
   description = "Fenix operating system";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";};
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ gcc-arm-embedded cargo qemu rustup ];
-        shellHook = ''
-          TARGET="armv7a-none-eabi"
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [gcc-arm-embedded cargo qemu rustup];
+      shellHook = ''
+        TARGET="armv7a-none-eabi"
 
-          if ! rustup target list --installed | grep -q $TARGET; then
-            rustup target add $TARGET
-          fi
+        if ! rustup target list --installed | grep -q $TARGET; then
+          rustup target add $TARGET
+        fi
 
-          # Just a convenience for myself
-          zsh
-        '';
-      };
-
+        # Just a convenience for myself
+        zsh
+      '';
     };
+  };
 }
